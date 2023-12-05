656cf64df867fe04f3eb984679f667d6d72afa56
Denys Chavez <dvchf26@gmail.com>
2023-12-05 11:02:20 -0500

    change logo format from svg to png

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+import classes from './index.module.scss'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants/inde'
+import { usePathname } from 'next/navigation'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+
+    const pathname = usePathname();
+
+  return (
+<nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(" ")}>
+    <Gutter className={classes.wrap}>
+        <Link href="/">
+            <Image src="/logo-quinn-wood.png" alt="logo" width={150} height={50}/>
+        </Link>
+
+        <HeaderNav header={header}/>
+    </Gutter>
+</nav>
+    )
+}
+
+export default HeaderComponent
