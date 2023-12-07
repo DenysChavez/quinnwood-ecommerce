f7531aa0dc7a75c80d7bba796f44f0ddebd090db
Denys Chavez <dvchf26@gmail.com>
2023-12-07 16:08:33 -0500

    update the style of the Card

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -170,7 +170,7 @@ export const CollectionArchive: React.FC<Props> = props =>
             {results.docs?.map((result, index) => {
               return (
                 
-                  <Card relationTo="products" doc={result} showCategories />
+                  <Card key={index} relationTo="products" doc={result} showCategories />
               )
             })}
           </div>
